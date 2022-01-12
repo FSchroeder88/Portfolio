cb14cd3fd78073adfcf04e139681f22d049f5341
Schröder <technoschroeder@web.de>
2022-01-12 07:43:51 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SlideshowComponent },
+  { path: 'impressum', component: ImpressumComponent },
+  { path: 'data-protection', component: DataProtectionComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { MenuComponent } from './menu/menu.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent,
+    MenuComponent,
+    ImpressumComponent,
+    DataProtectionComponent
   ],
   imports: [
     BrowserModule,
