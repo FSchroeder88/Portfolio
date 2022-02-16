b2ad6b062864a4375f435edd17acd3528a1425ce
Schröder <technoschroeder@web.de>
2022-02-16 19:50:24 +0100

    material buttons

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -6,29 +6,49 @@ import { Component, OnInit, } from '@angular/core';
   styleUrls: ['./mywork.component.scss']
 })
 export class MyworkComponent implements OnInit {
-  showMeall:boolean = false;
-  showMeJavascript:boolean = false;
-  showMeAngular:boolean = false;
+  showMeall: boolean = false;
+  showMeJavascript: boolean = false;
+  showMeAngular: boolean = false;
+  showMeallColor: boolean = false;
+  showMejsColor: boolean = false;
+  showMeangularColor: boolean = false;
+
 
   ngOnInit(): void {
-  
+
   }
-toogleTagall(){
-  this.showMeall = true;
-  this.showMeJavascript = true;
-  this.showMeAngular = true;
-}
 
-toogleTagJavascript(){
-  this.showMeJavascript = true;
-  this.showMeAngular = false;
-  this.showMeall = false;
-}
+  AppComponent() {
 
-toogleTagAngular(){
-  this.showMeAngular = true;
-  this.showMeall = false;
-  this.showMeJavascript = false;
-}
- 
+
+
+  }
+
+  toogleTagall() {
+    this.showMeallColor = true;
+    this.showMeall = true;
+    this.showMeJavascript = true;
+    this.showMeAngular = true;
+    this.showMejsColor = false;
+    this.showMeangularColor = false;
+  }
+
+  toogleTagJavascript() {
+    this.showMejsColor = true;
+    this.showMeJavascript = true;
+    this.showMeAngular = false;
+    this.showMeall = false;
+    this.showMeallColor = false;
+    this.showMeangularColor = false;
+  }
+
+  toogleTagAngular() {
+    this.showMeangularColor = true;
+    this.showMeAngular = true;
+    this.showMeall = false;
+    this.showMeJavascript = false;
+    this.showMeallColor = false;
+    this.showMejsColor = false;
+  }
+
 }
