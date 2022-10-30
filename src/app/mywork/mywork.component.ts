b608bae15ef325ea42258965c54237e0d71e7260
FSchroeder88 <96183878+FSchroeder88@users.noreply.github.com>
2022-10-30 13:17:05 +0100

    Update

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -12,6 +12,8 @@ export class MyworkComponent implements OnInit {
   showMeallColor: boolean = false;
   showMejsColor: boolean = false;
   showMeangularColor: boolean = false;
+  showMeDjango: boolean = false;
+  showMeDjangoColor: boolean = false;
 
 
   ngOnInit(): void {
@@ -20,8 +22,6 @@ export class MyworkComponent implements OnInit {
 
   AppComponent() {
 
-
-
   }
 
   toogleTagall() {
@@ -29,26 +29,44 @@ export class MyworkComponent implements OnInit {
     this.showMeall = true;
     this.showMeJavascript = true;
     this.showMeAngular = true;
+    this.showMeDjango = true;
     this.showMejsColor = false;
     this.showMeangularColor = false;
+    this.showMeDjangoColor = false;
   }
 
   toogleTagJavascript() {
     this.showMejsColor = true;
     this.showMeJavascript = true;
     this.showMeAngular = false;
+    this.showMeDjango = false;
     this.showMeall = false;
     this.showMeallColor = false;
     this.showMeangularColor = false;
+    this.showMeDjangoColor = false;
   }
 
   toogleTagAngular() {
     this.showMeangularColor = true;
     this.showMeAngular = true;
     this.showMeall = false;
+    this.showMeDjango = false;
     this.showMeJavascript = false;
     this.showMeallColor = false;
     this.showMejsColor = false;
+    this.showMeDjangoColor = false;
+  }
+
+  toogleTagDjango() {
+    this.showMeDjango = true;
+    this.showMeDjangoColor = true;
+    this.showMeangularColor = false;
+    this.showMeAngular = false;
+    this.showMeall = false;
+    this.showMeJavascript = false;
+    this.showMeallColor = false;
+    this.showMejsColor = false;
+    
   }
 
 }
